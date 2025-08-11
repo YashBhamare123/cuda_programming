1bbf640f7570f7eddd6f87c0eee88454b6e231a3
yash <thevoiceofyashbhamare@gmail.com>
2025-08-11 16:59:45 +0530

    Added Shared Memory Kernel

--- matmul_kernels/SMEM_kernel.cu
+++ matmul_kernels/SMEM_kernel.cu
@@ -13,30 +13,69 @@
 
 
 template <typename T>
-__global__ void multiply(float *A, float *B, float *C, int m, int n, int k, T blocksize){
+__global__ void multiply(float *A, float *B, float *C, int M, int N, int K, T blocksize){
 
 
     __shared__ float As[blocksize * blocksize];
     __shared__ float Bs[blocksize * blocksize];
 
-    const uint block_offset_x = blocksize * blocksize * blockIdx.x;
-    const uint block_offset_y = blockIdx.y * N * blocksize;
+    const uint block_offset_C = blockIdx.y * (N / blocksize) * blocksize * blocksize + blocksize * blockIdx.x;
+    const uint thread_offset_x = threadIdx.x % blocksize ;
+    const uint thread_offset_A_y = K * threadIdx.x / blocksize;
+    const uint thread_offset_B_y = N * threadIdx.x / blocksize;
 
-    const uint tid = block_offset_x + block_offset_y + threadIdx.x;
+    const uint block_offset_A = blockIdx.y * (K / blocksize) * blocksize * blocksize;
+    const uint block_offset_B = blockIdx.x * blocksize;
 
-    // Indexing for A
-    const uint bidA_y = block_offset_y = blockIdx.y * K * blocksize;
-    const uint bidA_x = blockIdx.x * blocksize;
-    const uint tidA_x = threadIdx.x % blocksize;
-    const uint tidA_y = (threadIdx.x / blocksize) * blocksize;
-    const uint tidA = bidA_x + bidA_y + tidA_x + tidA_y;
+    const uint C_index = block_offset_C + thread_offset_x;
 
-    // Indexing for B
-    const uint bidB_x = blockIdx.x * blocksize;
-    const uint bidB_y = blockIdx.y * N * blocksize;
-    const uint tidB_x = threadIdx.x % blocksize;
-    const uint tidB_y = (threadIdx.x / blocksize) * blocksize;
-    const uint tidB = bidB_x + bidB_y + tidB_x + tidb_y;
+    for (int i = 0; i < K / blocksize ; i += blocksize){
+        A_index = block_offset_A + i + thread_offset_A_y + thread_offset_x;
+        B_index = block_offset_B + i * N + thread_offset_B_y + thread_offset_x;
 
+        As[threadIdx.x] = A[A_index];
+        Bs[threadIdx.x] = B[B_index];
 
+        __syncthreads();
+        for (int j = 0; j < blocksize; j++){
+            C[C_index] = As[j + blocksize * (threadIdx.x / blocksize)]* Bs[j * blocksize + thread_offset_x];
+        }
+    }
+}
+
+int main(){
+    float *h_A, *h_B, *h_C, *d_A, *d_B, *d_C;
+    int m = 4, n = 4 , k = 4;
+    size_t size_A = sizeof(float) * m * k, size_B = sizeof(float) * k * n, size_C = sizeof(float) * m * n;
+    h_A = (float*)malloc(size_A);
+    h_B = (float*)malloc(size_B);
+    h_C = (float*)malloc(size_C);
+
+    for (int i = 0; i< m * k; i ++)
+        h_A[i] = 1;
+    for (int i = 0; i< n * k; i ++)
+        h_B[i] = 1;
+
+    CUDA_CHECK(cudaMalloc(&d_A, size_A));
+    CUDA_CHECK(cudaMalloc(&d_B, size_B));
+    CUDA_CHECK(cudaMalloc(&d_C, size_C));
+
+    CUDA_CHECK(cudaMemcpy(d_A, h_A, size_A, cudaMemcpyHostToDevice));
+    CUDA_CHECK(cudaMemcpy(d_B, h_B, size_B, cudaMemcpyHostToDevice));
+
+    int blocksize = 2;
+    dim3 dim = (n / blocksize, m /blocksize);
+    multiply<<<dim, blocksize*blocksize>>>(d_A, d_B, d_C, m, n, k, blocksize);
+
+    CUDA_CHECK(cudaMemcpy(h_C, d_C, size_C, cudaMemcpyDeviceToHost));
+    
+    for (int i = 0; i < m * n; i ++)
+        printf("%d\n", h_C[i]);
+    
+    free(h_A);
+    free(h_B);
+    free(h_C);
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
 }
